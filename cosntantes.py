e3e620789aefa2ef794d90d52a4dac1b131a2280
osvaldosoares-hub <osvaldo.jrjuniorlinhares@gmail.com>
2021-12-09 16:56:29 -0300

    organizando o codigo

---
+++ cosntantes.py
@@ -0,0 +1,17 @@
+import pygame
+LARGURA= 800
+ALTURA =600
+BRANCO=(255,255,255)
+PRETO=(0,0,0)
+TITULO='MADE AMERICA'
+
+#fps
+FPS= 30
+
+SPRITESHEET = 'man.png'
+LOGO = 'logodohomi.png'
+
+#fonte
+FONT = 'arial'
+MUSICA= 'Aula3_audios_intermission.WAV'
+TECLA_START ='Aula3_audios_munch_1.WAV'
---
+++ sprites.py
@@ -0,0 +1,3 @@
+sprite_sheet = pygame.image.load(os.path.join(diretorio_sprites,'man.png')).convert_alpha()
+diretorio_principal = os.path.dirname(__file__)
+diretorio_sprites = os.path.join(diretorio_principal,'sprites')
